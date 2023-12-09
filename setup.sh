a250d42b20ffe32e18b2a75951356aae4cd4dd27
ulsbai <140907693+ulsbai@users.noreply.github.com>
2023-12-08 18:36:26 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
+VERSION="1.0"
+
+echo INSTALLING EMPYREAN-($VERSION)
+
+echo BUILDING EMPYREAN
+
 python setup.py sdist
 
+echo CREATING VIRTUAL ENVIORMENT
+
 python -m venv .venv
+
+echo INSTALLING EMPYREAN INTO VITRUAL ENVIORNMENT
+
+.venv/bin/python -m pip install dist/empyrean-($VERSION).tar.gz
+
+echo VIRTUAL ENVIORNMENT CREATED FOR EMPYREAN, USE .venv/bin/activate TO ACTIVATE, deactivate TO DEACTIVATE
