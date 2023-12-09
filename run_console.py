a54fd2f6a91171cd5874fc815b46bf9f233085d9
ulsbai <140907693+ulsbai@users.noreply.github.com>
2023-12-09 09:57:22 -0800

    Update run_console.py

--- run_console.py
+++ run_console.py
@@ -1,11 +1,17 @@
 from empyrean.empyrean import Empyrean
+from termcolor import colored
 
 machine = Empyrean()
+
+def output(text, type):
+  assert type in ['intro', 'echo']
+  color = 'red' if type == 'intro' else 'green' if type == 'echo'
 
 machine.addOutputListener(output)
 machine.start()
 
 print('Running Empyrean...')
+print('Output will be in', colored('red for intro,', 'red'), 'and', colored('green for echo', 'green'))
 
 while True:
   prompt = input()
